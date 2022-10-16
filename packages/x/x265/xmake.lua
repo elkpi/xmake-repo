955c63f849a0e7fe99712bf5085aa8eaa6be65db
ping <elkpi@qq.com>
2022-10-16 23:01:49 +0800

    x265 support android

--- packages/x/x265/xmake.lua
+++ packages/x/x265/xmake.lua
@@ -8,6 +8,8 @@ package("x265")
 
     add_versions("3.4", "544d147bf146f8994a7bf8521ed878c93067ea1c7c6e93ab602389be3117eaaf")
 
+    add_patches("3.4", path.join(os.scriptdir(), "patches", "3.4", "android_cmake.patch"), "9ad6c4ca45a16a1e6db17048cc827c4ffcb8820c4cf136322722d277e1723120")
+
     add_deps("cmake", "nasm")
 
     if is_plat("macosx") then
@@ -22,6 +24,26 @@ package("x265")
         import("package.tools.make").make(package, {"-C", "8bit", "install", "DESTDIR=./" })
         os.vrun("sed -i 's#^prefix=.*#prefix=" .. package:installdir() .. "#g' 8bit/usr/local/lib/pkgconfig/x265.pc")
         os.cp("8bit/usr/local/*", package:installdir())
+    end)
+
+    on_install("android", "cross", function (package)
+        local configs = {
+            "../source",
+        }
+        if package:is_plat("android") then
+            table.insert(configs, "-DNEON_ANDROID=1")
+        else
+            if package:is_arch("arm.*64.*") then
+                table.insert(configs, "-DCROSS_COMPILE_ARM=1")
+                table.insert(configs, "-DCMAKE_SYSTEM_PROCESSOR=aarch64")
+            elseif package:is_arch("arm.*") then
+                table.insert(configs, "-DCROSS_COMPILE_ARM=1")
+                table.insert(configs, "-DCMAKE_SYSTEM_PROCESSOR=armv6l")
+            end
+        end
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
