1ba9a66f14009eb60e9565c6155a222ae2cee33f
ping <elkpi@qq.com>
2023-09-03 01:06:28 +0800

    add libks v2.0.2

--- packages/l/libks/xmake.lua
+++ packages/l/libks/xmake.lua
@@ -3,15 +3,24 @@ package("libks")
     set_description("Foundational support for signalwire C products")
 
     add_urls("https://github.com/signalwire/libks.git")
+
+    add_versions("v2.0.2", "423c68c92d1871e2c9cb83d974eac7830ddcdcf5")
     add_versions("v1.8.3", "607b4b34251400d9531e02a6612c32d2ab66ca95")
     add_versions("v1.8.0", "bccc2f394855500c8f6f488b441d6fb94343491b")
 
+    add_patches("v2.0.2", path.join(os.scriptdir(), "patches", "v2.0.2", "cmake.patch"), "a4387b707d32b8ba6976ab626fa983ef03bfa8d54885cdbffc2d70970a0c17ef")
     add_patches("v1.8.3", path.join(os.scriptdir(), "patches", "v1.8.3", "cmake.patch"), "a3a4c0614d2fb74a92a87eb46bc2a31fe73f98a2a59b2cc8bf9f917b98415a8b")
     add_patches("v1.8.0", path.join(os.scriptdir(), "patches", "v1.8.0", "cmake.patch"), "8a7021401aa25af82623a455331ee48de6b90bb2fad6611fb7ee17a51a593e20")
 
     add_deps("cmake")
     add_deps("pkg-config")
     add_deps("libuuid", "openssl")
+
+    on_load(function (package)
+        if package:version():major() == 2 then
+            package:add("includedirs", "include/libks2")
+        end
+    end)
 
     on_install(function (package)
         import("package.tools.cmake")
