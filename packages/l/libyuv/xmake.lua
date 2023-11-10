cf9c28907e43aa9ffa64e1258e098fc05fa24edf
ping <elkpi@qq.com>
2023-11-10 22:15:02 +0800

    适配新版本libyuv

--- packages/l/libyuv/xmake.lua
+++ packages/l/libyuv/xmake.lua
@@ -8,12 +8,13 @@ package("libyuv")
     add_urls("https://chromium.googlesource.com/libyuv/libyuv.git", {alias = "chromium"})
     add_urls("https://github.com/lemenkov/libyuv.git", {alias = "github"})
 
+    add_versions("elkpi:2023.11.10", "4aaf0f22fd75e21dbb344394185eab198237029d")
     add_versions("elkpi:2023.05.27", "dfee0d31e055b6a5d6b3fb26e861735d1b518c82")
     add_versions("chromium:2023.04.22", "6f4731cdbc7e8b3fae163256dd8a2437508264d4")
-    add_versions("github:2023.04.22", "6900494d90ae095d44405cd4cc3f346971fa69c9")
     add_versions("chromium:2023.10.27", "31e1d6f896615342d5d5b6bde8f7b50b3fd698dc")
+    add_versions("github:2023.04.22", "6900494d90ae095d44405cd4cc3f346971fa69c9")
 
-    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    add_configs("shared", {description = "Build shared library.", default = false, type = "boolean"})
 
     add_deps("cmake", "libjpeg-turbo")
 
@@ -21,7 +22,7 @@ package("libyuv")
     end)
 
     on_install("windows", "linux", "macosx", "android", "cross", "bsd", "mingw", function (package)
-        local configs = {"-DTEST=OFF"}
+        local configs = {"-DTEST=OFF", "-DUNIT_TEST=OFF"}
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         io.replace("CMakeLists.txt", "INSTALL ( PROGRAMS ${CMAKE_BINARY_DIR}/yuvconvert			DESTINATION bin )", "", {plain = true})
