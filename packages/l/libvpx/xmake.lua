37336377fd9722df6f37dcb7d2b1aa3111998e76
ping <elkpi@qq.com>
2024-07-07 00:07:58 +0800

    fix clang compile error

--- packages/l/libvpx/xmake.lua
+++ packages/l/libvpx/xmake.lua
@@ -8,7 +8,7 @@ package("libvpx")
 
     add_versions("github:v1.8.1", "8ae686757b708cd8df1d10c71586aff5355cfe1e")
     add_versions("github:v1.13.0", "d6eb9696aa72473c1a11d34d928d35a3acc0c9a9")
-    add_deps("nasm", "libyuv")
+    add_deps("yasm", "libyuv")
 
     on_load("windows", "linux", "macosx", function (package)
     end)
@@ -19,10 +19,8 @@ package("libvpx")
             "--disable-examples", "--disable-tools", "--disable-docs",
             "--disable-install-bins", "--disable-install-srcs",
             "--disable-unit-tests", "--disable-decode-perf-tests", "--disable-encode-perf-tests",
-            "--size-limit=16384x16384"
+            "--size-limit=16384x16384", "--as=yasm"
         }
-        local cflags = {}
-        local ldflags = {}
 
         if package:config("shared") then
             table.insert(configs, "--enable-shared")
@@ -36,9 +34,10 @@ package("libvpx")
             table.insert(configs, "--enable-pic")
         end
 
-        table.insert(configs, "--target=x86_64-linux-gcc")
-
-        import("package.tools.autoconf").install(package, configs, {cflags = cflags, ldflags = ldflags})
+        import("package.tools.autoconf")
+        local envs = autoconf.buildenvs(package)
+        envs.ASFLAGS = {} -- remove -m64
+        autoconf.install(package, configs, {envs = envs})
     end)
 
     on_test(function (package)
