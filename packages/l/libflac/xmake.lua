5bc3950d60e1db60ecee6e9995f09a7b1ccc0bc5
ping <elkpi@qq.com>
2024-09-08 16:11:47 +0800

    support cross

--- packages/l/libflac/xmake.lua
+++ packages/l/libflac/xmake.lua
@@ -33,7 +33,7 @@ package("libflac")
         end
     end)
 
-    on_install("windows", "linux", "macosx", "iphoneos", "mingw", "android", "wasm", function (package)
+    on_install("windows", "linux", "macosx", "iphoneos", "mingw", "android", "wasm", "cross", function (package)
 
         local configs = {}
         table.insert(configs, "-DBUILD_CXXLIBS=OFF")
