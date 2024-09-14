7106670560555ac1c3af46922ecbacddd1afebf7
ping <elkpi@qq.com>
2024-09-15 00:37:59 +0800

    cross CMAKE_POSITION_INDEPENDENT_CODE=ON

--- packages/l/leveldb/xmake.lua
+++ packages/l/leveldb/xmake.lua
@@ -16,6 +16,9 @@ package("leveldb")
 
     on_install("macosx", "linux", "windows", "mingw", "cross", function (package)
         local configs = {"-DLEVELDB_BUILD_TESTS=OFF", "-DLEVELDB_BUILD_BENCHMARKS=OFF"}
+        if package:is_plat("cross") then
+            table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
+        end
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
