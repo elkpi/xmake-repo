46bbe3bf0a5157e7b99509f4ab025452782df0c7
ping <elkpi@qq.com>
2024-09-15 12:16:47 +0800

    cross support

--- packages/f/folly/xmake.lua
+++ packages/f/folly/xmake.lua
@@ -67,7 +67,7 @@ package("folly")
         end
     end)
 
-    on_install("windows|x64", "macosx", "linux", function (package)
+    on_install("windows|x64", "macosx", "linux", "cross", function (package)
         local configs = {"-DBUILD_TESTS=OFF",
                          "-DCMAKE_DISABLE_FIND_PACKAGE_Libiberty=ON",
                          "-DCMAKE_DISABLE_FIND_PACKAGE_LibURCU=ON",
@@ -78,6 +78,18 @@ package("folly")
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         if package:is_plat("windows") then
             table.insert(configs, "-DBoost_USE_STATIC_RUNTIME=" .. (package:config("vs_runtime"):startswith("MT") and "ON" or "OFF"))
+        end
+        if package:is_plat("cross") then
+            if package:is_arch("arm64.*") then
+                table.insert(configs, "-DCMAKE_SYSTEM_PROCESSOR=aarch64")
+            elseif package:is_arch("arm.*") then
+                table.insert(configs, "-DCMAKE_SYSTEM_PROCESSOR=arm")
+            end
+            table.insert(configs, "-DFOLLY_HAVE_UNALIGNED_ACCESS=ON")
+            table.insert(configs, "-DFOLLY_HAVE_WEAK_SYMBOLS=ON")
+            table.insert(configs, "-DFOLLY_HAVE_LINUX_VDSO=ON")
+            table.insert(configs, "-DFOLLY_HAVE_WCHAR_SUPPORT=ON")
+            table.insert(configs, "-DHAVE_VSNPRINTF_ERRORS=ON")
         end
         import("package.tools.cmake").install(package, configs)
     end)
