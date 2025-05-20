faccd69b40e77fa9b6f7a46b8a96e0ed010bf1ec
ping <elkpi@qq.com>
2025-05-20 08:51:51 +0800

    brpc限制pb版本

--- packages/b/brpc/xmake.lua
+++ packages/b/brpc/xmake.lua
@@ -47,7 +47,7 @@ package("brpc")
             package:add("deps", "thrift")
         end
         if package:version():ge("1.12.0") then
-            package:add("deps", "protobuf-cpp", {configs = {zlib = true, shared = true}})
+            package:add("deps", "protobuf-cpp <26", {configs = {zlib = true, shared = true}})
         elseif package:version():ge("1.9.0") then
             package:add("deps", "protobuf-cpp <24", {configs = {zlib = true, shared = true}})
         else
