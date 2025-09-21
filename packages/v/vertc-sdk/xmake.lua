aacc1ae72a05fb40baf389770b5b769b9a24ddab
ping <elkpi@qq.com>
2025-09-21 15:21:23 +0800

    rename vertc-sdk

--- packages/v/vertc-sdk/xmake.lua
+++ packages/v/vertc-sdk/xmake.lua
@@ -1,4 +1,4 @@
-package("vertc")
+package("vertc-sdk")
     set_kind("library")
     set_homepage("hhttps://www.volcengine.com/docs/6348/75707")
     set_description("Volcengine Real Time Communication SDK")
