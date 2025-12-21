8217179b9e55bc53edbbbd49283e2af28a62ff0a
ping <elkpi@qq.com>
2025-12-21 16:06:02 +0800

    update 0.0.2

--- packages/j/joker/xmake.lua
+++ packages/j/joker/xmake.lua
@@ -4,6 +4,7 @@ package("joker")
     add_urls("https://code.elkpi.com/cpputil/joker.git")
 
     add_versions("0.0.1", "9f8adaac6fb444f6a00ecb09731e9c3c245451b4")
+    add_versions("0.0.2", "d927a03b9ab60dc629503e141d55e10ad937a0c0")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
