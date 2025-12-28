1b5134d134af1167b405a719a2a1edf58d2aac68
ping <elkpi@qq.com>
2025-12-28 18:11:27 +0800

    add 0.0.4

--- packages/j/joker/xmake.lua
+++ packages/j/joker/xmake.lua
@@ -6,6 +6,7 @@ package("joker")
     add_versions("0.0.1", "9f8adaac6fb444f6a00ecb09731e9c3c245451b4")
     add_versions("0.0.2", "d927a03b9ab60dc629503e141d55e10ad937a0c0")
     add_versions("0.0.3", "b7b020982b53978036e7fd6b806cfd3d70f3040a")
+    add_versions("0.0.4", "cb76adcc72ed284e60f39773036370034c775ab9")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
